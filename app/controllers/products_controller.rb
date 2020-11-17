e94c1cb16250098f434582d4e9314e6f2a6e6fd7
Violaine Rouhet <rouhet.violaine@hotmail.fr>
2020-11-17 13:56:19 +0100

    creation of methods for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,33 @@
+authorize @product
+
 class ProductsController < ApplicationController
+
+    def index
+        @products = Product.all
+    end
+
+    def show
+        @product = Product.find(params[:id])
+    end
+
+    def new
+        record.user == user || user.admin
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.find(params[:id])
+        @product.save
+            redirect_to products_path(id: @product.id)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def products_params
+        params.require(:product).permit(:name, :description, :price, :adress, :owner_id)
+    end
+
 end
