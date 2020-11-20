850cbfa6147e3c01eadffaf8211009d3edde8a44
Hugo Le Morvan <lm.hugo@hotmail.fr>
2020-11-20 11:19:07 +0100

    maj Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,47 +16,47 @@ User.destroy_all
 
 puts "Creating 3 users"
 
-user = User.new(
+user1 = User.new(
   first_name: "Eric",
   last_name: Faker::Name.last_name,
   address: Faker::Address.street_name,
   email: Faker::Internet.email,
   password: "abcdef"
 )
-user.save!
+user1.save!
 file = URI.open('https://www.sardiniauniqueproperties.com/wp-content/uploads/2015/10/square-profile-pic-2.jpg')
 user.photo.attach(io: file, filename: 'user1.jpg', content_type: 'image/jpeg')
 
-user = User.new(
+user2 = User.new(
   first_name: "Francis",
   last_name: Faker::Name.last_name,
   address: Faker::Address.street_name,
   email: Faker::Internet.email,
   password: "abcdef"
 )
-user.save!
+user2.save!
 file = URI.open('https://worldbusinessfitness.com/wp-content/uploads/2018/01/opulent-profile-square-07.jpg')
 user.photo.attach(io: file, filename: 'user2.jpg', content_type: 'image/jpeg')
 
-user = User.new(
+user3 = User.new(
   first_name: "Natasha",
   last_name: Faker::Name.last_name,
   address: Faker::Address.street_name,
   email: Faker::Internet.email,
   password: "abcdef"
 )
-user.save!
+user3.save!
 file = URI.open('https://worldbusinessfitness.com/wp-content/uploads/2018/06/opulent-profile-square-06.jpg')
 user.photo.attach(io: file, filename: 'user3.jpg', content_type: 'image/jpeg')
 
-user = User.new(
+user4 = User.new(
   first_name: "Jean-Michel",
   last_name: "Lopez",
   address: "12 rue de Tourcoing",
   email: "michou@mail.fr",
   password: "abcdef"
 )
-user.save!
+user4.save!
 file = URI.open('https://images.squarespace-cdn.com/content/v1/53af1c83e4b0b3e1fc2000bd/1446397787787-SI1H06DNOL2QJSX13DCD/ke17ZwdGBToddI8pDm48kOyctPanBqSdf7WQMpY1FsRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpwwQIrqN0bcqL_6-iJCOAA0qwytzcs0JTq1XS2aqVbyK6GtMIM7F0DGeOwCXa63_4k/Randy+Krum+Profile+Photo+square.jpg')
 user.photo.attach(io: file, filename: 'user4.jpg', content_type: 'image/jpeg')
 
@@ -67,7 +67,7 @@ puts "Users created"
 
 puts "Creating 5 products"
 
-user = User.find(2)
+user = user2
 product = Product.new(
   name: "Barbecue famillial",
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
@@ -79,7 +79,7 @@ product.save!
 file = URI.open('https://images-na.ssl-images-amazon.com/images/I/61ruYjKAARL._AC_SY450_.jpg')
 product.photo.attach(io: file, filename: 'barbecue1.jpg', content_type: 'image/jpeg')
 
-user = User.find(3)
+user = user3
 product = Product.new(
   name: "Racletzor 4000",
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
@@ -92,7 +92,7 @@ file = URI.open('https://www.gifi.fr/media/catalog/product/cache/1/image/9df78ea
 product.photo.attach(io: file, filename: 'raclette.jpg', content_type: 'image/jpeg')
 
 
-user = User.find(3)
+user = user3
 product = Product.new(
   name: "Barbecue à charbon",
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
@@ -104,7 +104,7 @@ product.save!
 file = URI.open('https://images-na.ssl-images-amazon.com/images/I/81iyL9dJtiL._AC_SX466_.jpg')
 product.photo.attach(io: file, filename: 'barbecue2.jpg', content_type: 'image/jpeg')
 
-user = User.find(2)
+user = user2
 product = Product.new(
   name: "Boite à outils",
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
@@ -117,7 +117,7 @@ file = URI.open('https://www.bricorama.fr/media/catalog/product/2/2/22cf49e2ebe0
 product.photo.attach(io: file, filename: 'outils.jpg', content_type: 'image/jpeg')
 
 
-user = User.find(1)
+user = user1
 product = Product.new(
   name: "Barbecue tout-en-un",
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
